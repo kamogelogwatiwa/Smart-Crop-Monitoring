5801da6c317b2e0c6ba704b69102f7aa6c8fd290
kamogelogwatiwa <164397501+kamogelogwatiwa@users.noreply.github.com>
2025-05-01 06:31:10 +0200

    Update smart_crop_monitoring_web_app.py

--- smart_crop_monitoring_web_app.py
+++ smart_crop_monitoring_web_app.py
@@ -54,9 +54,10 @@ with tab1:
             st.stop()
 
     # Model Prediction
+   if st.button("🔍 Predict Crop Health"):
     input_array = np.array([[soil_moisture, nitrogen_level, potassium_level]])
     prediction = health_model.predict(input_array)[0]
-    print(input_array)
+
     st.markdown(f"### 🧠 Predicted Crop Health Status: **{prediction}**")
 
     # --- RULE-BASED RECOMMENDATIONS ---
@@ -91,6 +92,7 @@ with tab1:
             st.error("Low potassium. Apply potash fertilizer.")
         else:
             st.warning("Plant under stress. Review environmental and soil conditions holistically.")
+
 """
 # -------------------- Crop Disease Detection Tab --------------------
 with tab2:
